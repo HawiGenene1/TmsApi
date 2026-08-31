4f848b1469fdd633c53765920ec194a0ec3224e9
HawiGenene1 <hawigenene2u@gmail.com>
2026-09-01 00:16:55 +0300

    Module 5 Session update

--- Entities/Enrollment.cs
+++ Entities/Enrollment.cs
@@ -8,6 +8,13 @@ public class Enrollment
     public decimal? Grade { get; set; }
     public DateTime EnrolledAt { get; set; } = DateTime.UtcNow;
     
+
+// NEW: Academic Year (e.g., "2024-2025")
+    public string? AcademicYear { get; set; }
+    
+    // NEW: Archive flag
+    public bool IsArchived { get; set; } = false;
+    
     // Navigation properties
     public Student Student { get; set; } = null!;
     public Course Course { get; set; } = null!;
--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6b1d4c4a189fab2382c3b827d0ac994477da162")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2987667a9bbea822fd22b9f6e5988799dfc3f122")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
