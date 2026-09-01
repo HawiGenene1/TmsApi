7da33800dce091104725ee295cd58fed232b2168
HawiGenene1 <hawigenene2u@gmail.com>
2026-09-01 15:38:22 +0300

    Complete M6 Lab Session 2: Scale and Cross-Cutting

--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0744c0949bcf7df7a061e4f6daadbbd37b4d9746")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b331332d8ea073162847f81a607571d4a9e4126a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
